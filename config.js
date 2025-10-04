6c19fe42de95b6acda9f1716407b8322093132eb
Divins021 <dimslegend649@gmail.com>
2025-10-04 18:41:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "INCONNU~XD~OkwlhLKI#nSKq5-oOWQwhAhY5xQcnxcGYYKnCHm2_9xxtaq6lzmc",
+SESSION_ID: process.env.SESSION_ID || "INCONNU~XD~eh40BSKD#YWWppyp8DGfyHp_Mmk1Bzx11LvGgiBaanB-73_qgwtw",
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
